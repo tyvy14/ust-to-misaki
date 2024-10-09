627975dc9d44d0b0421f63b15763d0d2555a2ed1
tyvy14 <116800692+tyvy14@users.noreply.github.com>
2024-10-09 05:32:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
-APP = ['seu_script.py']  # Substitua pelo nome do seu script
+APP = ['main.py']  # Certifique-se de que o nome do script principal está correto
 DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['tkinter'],  # Inclua tkinter, pois ele é necessário para a interface
+    'packages': ['os'],
 }
 
 setup(
-    app=APP,
+    app=APP,  # Use 'app' e não 'APP' aqui
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
+
